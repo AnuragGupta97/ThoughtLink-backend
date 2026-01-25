2dcc4ec6a370c11f47997bc26ced46f094fb4e08
Anurag Gupta <anuraggupta97@gmail.com>
2026-01-25 20:01:47 +0530

    email service updated: shift from gmail to resend

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -1,25 +1,26 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendOtpEmail(toEmail, otp) {
-    await transporter.sendMail({
-        from: `"My App" <${process.env.EMAIL_USER}>`,
-        to: toEmail,
-        subject: "Your OTP Verification Code",
-        html: `
-            <h2>Email Verification</h2>
-            <p>Your OTP is:</p>
-            <h1>${otp}</h1>
-            <p>This OTP will expire in 5 minutes.</p>
-        `
+  try {
+    await resend.emails.send({
+      from: "My App <onboarding@resend.dev>",
+      to: toEmail,
+      subject: "Your OTP Verification Code",
+      html: `
+        <h2>Email Verification</h2>
+        <p>Your OTP is:</p>
+        <h1>${otp}</h1>
+        <p>This OTP will expire in 5 minutes.</p>
+      `,
     });
+
+    console.log("OTP sent via Resend");
+  } catch (error) {
+    console.error("Resend error:", error);
+    throw error;
+  }
 }
 
 module.exports = sendOtpEmail;
